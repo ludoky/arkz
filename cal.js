aac5a3c03c3cda7fc670188f04dca46a09dab708
Nicolas dos santos <69695834+ludoky@users.noreply.github.com>
2021-07-04 20:29:06 -0300

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,23 @@
+function calc() {
+    let usd=document.getElementById('usd');
+    let uyu=document.getElementById('uyu');
+    let uusd=usd.value;
+    let pusd=42.6;
+
+    uyu.value=(uusd*pusd);
+}
+
+function limp() {
+    let usd=document.getElementById('usd');
+    let uyu=document.getElementById('uyu');
+
+    usd.value=null;
+    uyu.value=null;
+}
+
+
+
+function cambio() {
+  
+
+}
